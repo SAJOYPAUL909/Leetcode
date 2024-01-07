02c46f94c16b36aff94ae2ec4c1e0c2c75eb927c
SajoyPaul <sajoypaul909@gmail.com>
2024-01-07 16:04:26 +0530

    274. H-Index

---
+++ Medium/H-Index.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i)
+                return citations.length - i;
+        }
+        return 0;
+    }
+}
