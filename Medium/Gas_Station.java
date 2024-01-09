2bf49f6fc33af2db914a8647bfd3897c913e6581
SajoyPaul <sajoypaul909@gmail.com>
2024-01-09 11:10:55 +0530

    134. Gas Station

---
+++ Medium/Gas_Station.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tank = 0;
+        int ans = 0;
+        int arr[] = new int[gas.length];
+        int gassum = Arrays.stream(gas).sum();
+        int costsum = Arrays.stream(cost).sum();
+
+        if (costsum > gassum) {
+            return -1;
+        }
+        for (int i = 0; i < gas.length; i++) {
+            tank = tank + gas[i] - cost[i];
+            if (tank < 0) {
+                tank = 0;
+                ans = i + 1;
+            }
+
+        }
+        return ans;
+    }
+}
