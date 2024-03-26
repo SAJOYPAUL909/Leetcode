ccdadcc857b8069a331d265b0c3cab4ec6e5d7d7
sajoypaul <sajoypaul909@gmail.com>
2024-03-26 08:35:15 +0530

    9. Palindrome Number

---
+++ Easy/Palindrome_Number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        if (x < 0)
+            return false ;
+        
+        int ans = 0;
+        int y = x;
+
+        while (y > 0)
+        {
+            ans = ans * 10 + y%10;
+            y /= 10;  
+        }
+        return ans == x;
+    }
+}
