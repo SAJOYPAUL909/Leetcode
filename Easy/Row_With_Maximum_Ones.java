cc9b3300b571828a155d2dea2f0bd11267a6538a
sajoypaul <sajoypaul909@gmail.com>
2024-04-22 08:07:18 +0530

    Row With Maximum Ones

---
+++ Easy/Row_With_Maximum_Ones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] rowAndMaximumOnes(int[][] mat) 
+    {
+        int ans[] = new int[2];
+
+        for (int i = 0 ; i < mat.length ; i++)
+        {
+            int one = 0;
+            for (int  j = 0 ; j < mat[0].length ; j++)
+            {
+                if (mat[i][j] == 1)
+                    one++;
+            }
+            
+            if (one > ans[1])
+            {
+                ans[0] = i;
+                ans[1] = one;
+            }
+        }
+        return ans;
+    }
+}
