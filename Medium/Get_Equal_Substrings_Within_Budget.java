0da128cd6d0c8b390160176a9241c8a228080ef6
sajoypaul <sajoypaul909@gmail.com>
2024-05-29 10:32:25 +0530

    1208. Get Equal Substrings Within Budget

--- Medium/Get_Equal_Substrings_Within_Budget.java
+++ Medium/Get_Equal_Substrings_Within_Budget.java
@@ -1,10 +1,8 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
         int a = 0;
-
         for (int i = 0; i < s.length(); i++) {
             maxCost -= Math.abs(s.charAt(i) - t.charAt(i));
-
             if (maxCost < 0)
                 maxCost += Math.abs(s.charAt(a) - t.charAt(a++));
         }
